c03116a22bc64f66628b2e596b22266bc767cdcc
P-hinn <40062588+P-hinn@users.noreply.github.com>
2020-04-26 10:26:33 +0200

    Add Tracks to Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,19 +6,50 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      searchResults: [{
+        name: 'testname',
+        artist: 'testartist',
+        album: 'testalbum',
+        id: 'testid'
+      }],
+      playlistName: 'Playlistname1',
+      playlistTracks: [{
+        name: 'trackplaylist',
+        artist: 'artistplaylist',
+        album: 'albumplaylist',
+        id: 'idplaylist'
+      }]
+    }
+
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    if(this.state.playlistTracks.find(trackelement => {trackelement.id === track.id})){
+      this.setState({playlistTracks: this.state.playlistTracks.push(track)});
+    }
+  }
+  
+  render() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
         </div>
       </div>
     </div>
   );
+  }
 }
 
 export default App;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -4,15 +4,21 @@ import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component{
+    constructor(props){
+        super(props);
+    }
 
     render() {
         return (
             <div className="TrackList">
-                {/* <!-- You will add a map method that renders a set of Track components  --> */}
-                <Track />
+                {
+                this.props.tracks.map(track => {
+                    return <Track track={track} key={track.id} onAdd={this.props.onAdd}/>
+                })
+                }
             </div>
         );
     }
 }
 
-export default SearchBar;
+export default TrackList;
